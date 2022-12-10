f298c8593e85c68dd7ed889e419e592552896132
Alessandro Giaquinto <alessandro.giaquinto87@gmail.com>
2022-12-10 23:01:06 +0100

    Solution1480

---
+++ LeetCode/Java/Solution1480.java
@@ -0,0 +1,26 @@
+package LeetCode.Java;
+
+import utils.Java.Utils;
+
+public final class Solution1480 {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{1, 2, 3, 4};
+        Utils.printArray(arr, false);
+        Utils.printArray(new Solution1480().runningSum(arr), false);
+    }
+
+    public Integer[] runningSum(Integer[] nums) {
+        Integer[] result = new Integer[nums.length];
+        int eval = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            eval = 0;
+            for (int j = 0; j <= i; j++) {
+                eval += nums[j];
+            }
+            result[i] = eval;
+        }
+
+        return result;
+    }
+}
